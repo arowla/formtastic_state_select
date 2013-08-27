27248de7997af559f2d413ec4a934cb0dc26133c
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-27 13:46:52 -0400

    Trying to adhere to Formtastic instructions. Probably will not work this time around.

--- lib/state_select_input.rb
+++ lib/state_select_input.rb
@@ -1,17 +1,11 @@
-module Formtastic
-  module Inputs
-
-    class StateInput
-      include Formtastic::Inputs::Base
-      def to_html
-        raise "To use the :state input, please install the state_select plugin http://github.com/sprsquish/state_select" unless builder.respond_to?(:state_select)
-        input_wrapping do
-          label_html <<
-          country = input_options.delete(:country) || 'US'
-          builder.state_select(method, country, input_options, input_html_options)
-        end
-      end
+class StateInput
+  include Formtastic::Inputs::Base
+  def to_html
+    raise "To use the :state input, please install the state_select plugin http://github.com/sprsquish/state_select" unless builder.respond_to?(:state_select)
+    input_wrapping do
+      label_html <<
+      country = input_options.delete(:country) || 'US'
+      builder.state_select(method, country, input_options, input_html_options)
     end
-
   end
 end
